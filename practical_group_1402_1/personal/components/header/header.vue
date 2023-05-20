9bcf4c4a3bbeccc6838e1a1337520046fdae80ed
Msdr84 <131704194+Msdr84@users.noreply.github.com>
2023-05-20 00:50:32 -0700

    Revert "header"
    This reverts commit 32ed1087d8f7b59bdb5b9e101aca76ea7042c918.

--- practical_group_1402_1/personal/components/header/header.vue
+++ practical_group_1402_1/personal/components/header/header.vue
@@ -1,36 +1,9 @@
 <template>
-  <div>
-    <div class="border text-center  p-5 mb-5   rounded-5">
-      <h1 class=" title ">
-        <p>وبسایت شخصی مطالعاتی</p>
-      </h1>
-      <hr>
-      <div class="p-1"><p>
-        <a class="link-dark  text-decoration-none fs-5 ms-4" href="/homme">
-           خانه
-        </a>
-        <a class="link-dark  text-decoration-none fs-5 ms-4" href="/samples">
-          نمونه کارها
-        </a>
-        <a class="link-dark  text-decoration-none fs-5 ms-4" href="/contact">
-          تماس با من
-        </a>
-        <a class="link-dark  text-decoration-none fs-5 ms-4" href="/blog">
-          وبلاگ
-        </a>
-        <a class="link-dark  text-decoration-none fs-5 ms-4" href="/about">
-          درباره من
-        </a>
-        <a class="link-dark  text-decoration-none fs-5 ms-4" href="/skills">
-          مهارت های من
-        </a>
-      </p>
-      </div>
-    </div>
-  </div>
+
 </template>
-<style>
-.border {
-  box-shadow: 2px 30px 20px 2px rgba(0, 0, 0, 0.3);
-}
+<script>
+
+</script>
+<style scoped>
+
 </style>
